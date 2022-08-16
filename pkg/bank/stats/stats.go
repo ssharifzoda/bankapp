eb76bba99b4813b2c3da6aa849dbf2f23b6cc389
Sino <sharifzoda.sino@gmail.com>
2022-08-16 18:27:31 +0000

    changed

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -4,10 +4,8 @@ import "github.com/ssharifzoda/bank/pkg/bank/types"
 
 func Avg(payments []types.Payment) types.Money {
 	var sum int
-	znam := 0
 	for _, payment := range payments {
 		sum += int(payment.Amount)
-		znam++
 	}
-	return types.Money(sum / znam)
+	return types.Money(sum / len(payments))
 }
