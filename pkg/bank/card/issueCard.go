15b73268b641a5ad674e33b6293a6282307bc6cd
Sino <sharifzoda.sino@gmail.com>
2022-08-16 01:42:23 +0000

    добавлена функция создания карт

--- pkg/bank/card/issueCard.go
+++ pkg/bank/card/issueCard.go
@@ -1,1 +1,16 @@
 package card
+
+import "github.com/ssharifzoda/bank/pkg/bank/types"
+
+func IssueCard(currency types.Currency, color string, name string) types.Card {
+	card := types.Card{
+		ID:       1000,
+		PAN:      "5058 xxxx xxxx 0001",
+		Balance:  0,
+		Currency: currency,
+		Color:    color,
+		Name:     name,
+		Active:   true,
+	}
+	return card
+}
