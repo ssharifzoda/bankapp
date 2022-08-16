dd801cab80a3823d875aa8f3e04e92ffe9cd7a8b
Sino <sharifzoda.sino@gmail.com>
2022-08-16 01:54:30 +0000

    д

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -1,8 +1,7 @@
 package card
 
 import (
-	"bank/pkg/bank/types"
-	"fmt"
+	"github.com/ssharifzoda/bank/pkg/bank/types"
 )
 
 func IssueCard(currency types.Currency, color string, name string) types.Card {
@@ -16,43 +15,4 @@ func IssueCard(currency types.Currency, color string, name string) types.Card {
 		Active:   true,
 	}
 	return card
-}
-func Deposit(card *types.Card, amount types.Money) {
-	const depositLimit = 50_000
-	if card.Active != false && amount <= depositLimit {
-		card.Balance += amount
-	}
-}
-func AddBonus(card *types.Card, percent int, daysInMonth int, daysInYear int) {
-	const bonusLimit = 5000
-	bonus := (int(card.MinBalance) * (percent) * (daysInMonth) / 100) / (daysInYear)
-	if bonus > bonusLimit {
-		bonus = bonusLimit
-	}
-	if card.Active != false && card.Balance > 0 {
-		card.Balance += types.Money(bonus)
-	}
-}
-func Total(cards []types.Card) types.Money {
-	var sum int
-	for _, card := range cards {
-		if int(card.Balance) < 0 && card.Active == false {
-			continue
-		}
-		sum += int(card.Balance)
-	}
-	return types.Money(sum)
-}
-func PaymentSources(cards []types.Card) []types.PaymentSource {
-	paymentSource := make([]types.PaymentSource, len(cards))
-	for i, card := range cards {
-		if card.Balance > 0 && card.Active != false {
-			paymentSource[i].Balance = card.Balance
-			paymentSource[i].Type = "card"
-			paymentSource[i].Number = string(card.PAN)
-		}
-		fmt.Println(paymentSource[i].Number)
-		return paymentSource
-	}
-	return paymentSource
 }
